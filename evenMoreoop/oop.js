0ca58c0b52c51e652a292e26b0b1ab6f52410774
Thomas Erickson <terickson@spotxchange.com>
2016-11-19 17:13:07 -0700

    becoming a little clearer

--- evenMoreoop/oop.js
+++ evenMoreoop/oop.js
@@ -1,15 +1,32 @@
 class Monepok {
-  constructor(name,type,battleStats){
+  constructor(name,type,battleStats){ //this initializes all incoming instances with these properties. They can either be passed in as parameters or they can be initialized on their own like the hp property.
     this.name = name;
     this.type = type;
     this.battleStats = battleStats;
     this.hp = battleStats['maxHp'];
+  }
+  takeDamage(points){
+    this.points = battleStats['attackPoints'];
+    this.hp -= this.points;
+  }
+  isFainted(){
+    if(this.hp > 0)
+      return false;
+    else
+      return true;
   }
 }
 
 class Shield {
   constructor(strength){
     this.strength = strength;
+  }
+  applyDefense(attackPoints){
+    this.strength *= attackPoints;
+    return this.strength;
+  }
+  withString(){
+    return 'with Shield';
   }
 }
 
@@ -21,4 +38,6 @@ class Weapon {
 }
 
 var billybob = new Monepok('billybob','fire',{maxHp: 90, attack: 40});
-console.log(billybob['hp']);
+console.log(billybob['isFainted']());
+var shield1 = new Shield(.75);
+console.log(shield1.applyDefense(100));
