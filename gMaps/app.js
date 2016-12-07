a2969211992ad15c5839d300df40dc363580f6d8
Thomas Erickson <terickson@spotxchange.com>
2016-12-07 13:40:16 -0700

    cant figure out why my screen is getting blanked

---
+++ gMaps/app.js
@@ -0,0 +1,18 @@
+function initMap() {
+  var mapZoomIn = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -13.1631412,
+      lng: -72.5471516
+    },
+    zoom: 14
+  });
+
+  // var mapZoomOut = new google.maps.Map(document.getElementById('map2'), {
+  //   center: {
+  //     lat: -13.1631412,
+  //     lng: -72.5471516
+  //   },
+  //   zoom: 5
+  // });
+
+}
