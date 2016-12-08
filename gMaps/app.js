9474f6baa5a08424fa9c0f3d1247ca4633ff6164
Thomas Erickson <terickson@spotxchange.com>
2016-12-08 13:14:17 -0700

    finished the stupid thing

--- gMaps/app.js
+++ gMaps/app.js
@@ -1,18 +1,13 @@
 function initMap() {
-  var mapZoomIn = new google.maps.Map(document.getElementById('map'), {
+  console.log(document.getElementById('map'));
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: {
-      lat: -13.1631412,
-      lng: -72.5471516
+      lat: 40.001479,
+      lng: -105.262259
     },
-    zoom: 14
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    heading: 45
   });
-
-  // var mapZoomOut = new google.maps.Map(document.getElementById('map2'), {
-  //   center: {
-  //     lat: -13.1631412,
-  //     lng: -72.5471516
-  //   },
-  //   zoom: 5
-  // });
-
+  console.log('printed');
 }
